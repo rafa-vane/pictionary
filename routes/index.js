4188766f88a0d1f340a77130de9c67d4e1e20eec
Vanesa Puerta <Vanesa@MacBook-Air-de-Vanesa.local>
2019-07-08 20:14:31 +0200

    nav, pagina game

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,21 @@ router.get('/userPage', (req, res, next) => {
   res.render('userPage', { user: req.user });
 });
 
+router.get(('/game-page'),(req, res, next) =>{
+  res.render('game-page');
+})
+
+// router.post("/game-page"),(req, res, next) => {
+//   Game
+//   .create({
+    
+//   })
+//   .then(()=>{
+//      res.render("/game-page")
+//   })
+//   .catch((err) =>console.log(err))
+
+// }
+
 
 module.exports = router;
