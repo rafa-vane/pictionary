585b116a9e82666fa945ab76ed625650790a9f91
Rafael Ruiz <rafaelruizgallardon@hotmail.com>
2019-07-10 17:34:31 +0200

    populateffffinished

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const Game = require("../models/Game");
 const User = require("../models/User");
 
@@ -12,33 +12,40 @@ router.get('/userPage', (req, res, next) => {
   res.render('userPage', { user: req.user });
 });
 
-router.get(('/gamePage/:id'),(req, res, next) =>{
+router.get(('/gamePage/:id'), (req, res, next) => {
   Game
-  .findById(req.params.id)
-  .then(game => {
-    
-    res.render('gamePage', {game});
-  })
-  
+    .findById(req.params.id)
+    .populate('creator')
+    .populate('guest')
+    .then(game => {
+      console.log(game)
+      res.render('gamePage', { game });
+    })
+
 })
 
-router.post("/gamePage",(req, res, next) => {
-  User.findOne({username: req.body.guest})
-  .then((guest) => {
-    Game
-    .create({
-      participants:{ 
-        creator: req.user,
-        guest: guest
-      },
-      winner: ""  
-    })
-    .then(game =>{
-       res.redirect(`gamePage/${game._id}`)
-    })
-    .catch((err) =>console.log(err))
-  })
-  .catch((err) =>console.log(err))
+router.post("/gamePage", (req, res, next) => {
+  User.findOne({ username: req.body.guest })
+    .then((guest) => {
+      let invited = guest._id;
+      let creator = req.user._id
+      Game
+        .create({
+          creator: req.user._id,
+          guest: guest._id,
+          winner: ""
+        })
+        .then(created => {
+          let gameId = created._id
+          User.findByIdAndUpdate(creator, { $push: { createdGames: gameId } }, { new: true })
+            .then(creator => {
+              User.findByIdAndUpdate(invited, { $push: { invitedGames: gameId } }, { new: true })
+                .then(paco => {
+                  res.redirect(`gamePage/${gameId}`)
+                }).catch(err => console.log(err))
+            }).catch(err => console.log(err))
+        }).catch((err) => console.log(err))
+    }).catch((err) => console.log(err))
 });
 
 
