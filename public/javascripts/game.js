c88ef039e931e6b5bf8bcc6e0ba92189a0457905
Rafael Ruiz <rafaelruizgallardon@hotmail.com>
2019-07-12 11:03:46 +0200

    canvas and axios implementations

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -39,7 +39,8 @@ class Game {
     this.erase()
     this.intervalId = setInterval(() => {
       this.save()
-    }, 1000 / 60)
+      this.drawGuest()
+    }, 1600)
   }
 
   eventListers = () => {
@@ -96,18 +97,33 @@ class Game {
   }
 
   save = () => {
-    // this.canvasDOMEl.toBlob(function(blob) {
-    //   let formData = new FormData(); 
-    //   formData.append("canvasImg", blob);  
-    // });
+ 
     this.dataURL = this.canvasDOMEl.toDataURL();
     document.getElementById("canvasImg").src = this.dataURL;
 
     this.idImage = (this.canvasDOMEl.baseURI).split("/")[4];
 
-    axios.post(`http://localhost:3000/canvasImg/${this.idImage}`, this.dataURL)
-      // .then(res => { console.log("Test res:", res) })
+    axios.post(`http://localhost:3000/canvasImg/${this.idImage}`, this.dataURL) 
+   
   }
+  
+
+drawGuest = () => {
+  this.idImage = (this.canvasDOMEl.baseURI).split("/")[4];
+  axios.get((`/gamePage/${this.idImage}`), (req, res, next) => {
+    Game
+      .findById(this.idImage)
+      .then((imagen)=> {
+        debugger
+        // console.log('ritaaaaaaa')
+        // console.log(imagen)
+       // document.getElementById("canvasImg").src = imagen
+      })
+
+  })
+}
+
+
 
 
   findxy = (res, e) => {
